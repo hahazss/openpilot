d4e74aa9f528bc63b5f12e9386241e9a47dcaa3f
multikyd <multikyd@gmail.com>
2025-02-17 21:38:12 +0900

    test

--- panda/board/safety/safety_hyundai_canfd.h
+++ panda/board/safety/safety_hyundai_canfd.h
@@ -223,8 +223,8 @@ static int hyundai_canfd_fwd_hook(int bus_num, int addr) {
   }
   if (bus_num == 2) {
     // LKAS for HDA2, LFA for HDA1
-    int hda2_lfa_block_addr = hyundai_canfd_hda2_alt_steering ? 0x362 : 0x2a4;
-    bool is_lkas_msg = ((addr == hyundai_canfd_hda2_get_lkas_addr()) || (addr == hda2_lfa_block_addr)) && hyundai_canfd_hda2;
+    // int hda2_lfa_block_addr = hyundai_canfd_hda2_alt_steering ? 0x362 : 0x2a4;
+    // bool is_lkas_msg = ((addr == hyundai_canfd_hda2_get_lkas_addr()) || (addr == hda2_lfa_block_addr)) && hyundai_canfd_hda2;
     // bool is_lfa_msg = ((addr == 0x12a) && !hyundai_canfd_hda2);
 
     // HUD icons
@@ -237,7 +237,7 @@ static int hyundai_canfd_fwd_hook(int bus_num, int addr) {
     bool is_angle_msg = ((addr == 0xcb) && !hyundai_canfd_hda2);
 
     // bool block_msg = is_lkas_msg || is_lfa_msg || is_lfahda_msg || is_scc_msg || is_angle_msg;
-    bool block_msg = is_lkas_msg || is_scc_msg || is_angle_msg;
+    bool block_msg = is_scc_msg || is_angle_msg;
 
     if (!block_msg) {
       bus_fwd = 0;
