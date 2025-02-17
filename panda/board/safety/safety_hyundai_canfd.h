f01568b42aa7d02e702af6906a0eabe145d76129
multikyd <multikyd@gmail.com>
2025-02-17 21:24:53 +0900

    test

--- panda/board/safety/safety_hyundai_canfd.h
+++ panda/board/safety/safety_hyundai_canfd.h
@@ -338,7 +338,7 @@ static safety_config hyundai_canfd_init(uint16_t param) {
 
       ret = hyundai_canfd_hda2_alt_steering ? BUILD_SAFETY_CFG(hyundai_canfd_hda2_rx_checks, HYUNDAI_CANFD_HDA2_ALT_STEERING_TX_MSGS) : \
                                               BUILD_SAFETY_CFG(hyundai_canfd_hda2_rx_checks, HYUNDAI_CANFD_HDA2_TX_MSGS);
-    } else if (!hyundai_camera_scc) {
+    } else if (!hyundai_camera_scc || true) {
       static RxCheck hyundai_canfd_radar_scc_alt_buttons_rx_checks[] = {
         HYUNDAI_CANFD_COMMON_RX_CHECKS(0)
         HYUNDAI_CANFD_ALT_BUTTONS_ADDR_CHECK(0)
